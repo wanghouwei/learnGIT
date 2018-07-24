93780c6099ba45c6615b51cbe007c4fe84fd9be5
whw <1026705785@qq.com>
2018-07-24 09:19:08 +0800

    the first one

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int *p = new int[20*10];
+    p[20*i+j] = 1;
+    *(p+20*j+i) = 1;
+
+    int **p = new int*[20];
+    for(int i = 0;i<=20;i++)
+    {
+        p[i] = new int[10];
+    }
+
+    vector<vector<int>> array(m,vector<int>(n,0));
+    array[i][j] = 0;
+    string b = '0'+a;
+    string s = a.substr(0,a.length());
+    abs();
+    fabs();
+    sqrt();
+    pow(a,c);
+    stringstream b;
+    string a;
+    while(cin>>a)
+    {
+        b = cin.get();
+        b<<a;
+
+        string b = "000"+a;
+        string c = a+'0';
+        cout<<b<<endl<<c<<endl;
+    }
+    return 0;
+}
